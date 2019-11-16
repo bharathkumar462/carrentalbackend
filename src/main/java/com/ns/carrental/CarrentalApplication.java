17d2c357f526c87dc4e982a2a8c23ffd87bc9022
budayakumar <budayakumar@nextsphere.com>
2019-11-16 18:42:17 +0530

    updatenov16

--- src/main/java/com/ns/carrental/CarrentalApplication.java
+++ src/main/java/com/ns/carrental/CarrentalApplication.java
@@ -1,13 +1,13 @@
 package com.ns.carrental;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarrentalApplication {
-	private static final Logger logger= LoggerFactory.getLogger(CarrentalApplication.class);
+	private static final Logger logger = LogManager.getLogger(CarrentalApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CarrentalApplication.class, args);
 
